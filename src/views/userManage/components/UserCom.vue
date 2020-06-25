4b98876a92ce737fd628b46f9e816f726c7bebe9
zogeli100 <418340696@qq.com>
2020-06-26 00:25:23 +0800

    用户信息

---
+++ src/views/userManage/components/UserCom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="label">{{ msg.label }}</div>
+    <div class="value">{{ msg.value }}</div>
+    <div style="clear:both" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg']
+}
+</script>
+<style scoped>
+.label {
+  width: 200px;
+  float: left;
+}
+.value {
+  float: left;
+}
+</style>
--- src/views/userManage/user-Detail.vue
+++ src/views/userManage/user-Detail.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="tab-container">
+  <div class="tab-container" @click="test()">
 
+    <el-row :gutter="20">
+      <el-col v-for="item in userMap" :span="12">
+        <UserCom :msg="item" />
+      </el-col>
+    </el-row>
 
-    <label class="radio-label" style="padding-left:0;">{{ $t('usermanagetip.tip1') }}</label>
+    <!-- <label class="radio-label" style="padding-left:0;">{{ $t('usermanagetip.tip1') }}</label>
     <label class="radio-label" style="padding-left:200;">{{ $t('usermanagetip.tip1') }}</label>
 
     <label class="radio-label" style="padding-left:400;">{{ $t('usermanagetip.tip17') }}</label>
@@ -21,10 +26,15 @@
     <label class="radio-label" style="padding-left:100;">{{ $t('usermanagetip.tip1') }}</label>
     <br/>
     <label class="radio-label" style="padding-left:100;">{{ $t('usermanagetip.tip8') }}</label>
-    <label class="radio-label" style="padding-left:100;">{{ $t('usermanagetip.tip1') }}</label>
+    <label class="radio-label" style="padding-left:100;">{{ $t('usermanagetip.tip1') }}</label>-->
 
     <el-tabs v-model="activeName" style="margin-top:15px;" type="border-card">
-      <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+      <el-tab-pane
+        v-for="item in tabMapOptions"
+        :key="item.key"
+        :label="item.label"
+        :name="item.key"
+      >
         <keep-alive>
           <tab-pane v-if="activeName==item.key" :type="item.key" @create="showCreatedTimes" />
         </keep-alive>
@@ -35,15 +45,25 @@
 
 <script>
 import TabPane from './components/TabPane'
+import UserCom from './components/UserCom'
 
 export default {
   name: 'Tab',
-  components: { TabPane },
+  components: { TabPane, UserCom },
   data() {
     return {
       tabMapOptions: [
         { label: '游戏记录', key: 'CN' },
         { label: '获得奖励', key: 'US' }
+      ],
+      userMap: [
+        { label: '用户名', value: '张三' },
+        { label: '上次登录时间', value: '2020-04-28 18:23:15' },
+        { label: '手机号', value: '15868126666' },
+        { label: '创建时间', value: '2020-04-28 18:23:15' },
+        { label: '万里通积分', value: '23465' },
+        { label: '状态', value: '正常' },
+        { label: '口袋金币', value: '238' }
       ],
       activeName: 'CN',
       createdTimes: 0
@@ -64,13 +84,26 @@ export default {
   methods: {
     showCreatedTimes() {
       this.createdTimes = this.createdTimes + 1
+    },
+    test() {
+      this.$set(this.userMap, 0, { label: '口袋金币', value: '238' })
+      console.log(this.userMap)
     }
   }
 }
 </script>
 
 <style scoped>
-  .tab-container {
-    margin: 30px;
-  }
+.tab-container {
+  margin: 30px;
+}
+.el-col {
+  line-height: 40px;
+}
+.el-row {
+  width: 800px;
+}
+.el-col > span {
+  padding-left: 40px;
+}
 </style>
